9a9c2e3a086b25c9f5053a05cd59f340b88dd3f4
Nikhil Mehra <nikhilmehra845@gmail.com>
2025-11-09 20:15:34 +0100

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -14,9 +14,10 @@ import os
 
 from src.utils import save_object
 
+
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -122,3 +123,5 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
+    
