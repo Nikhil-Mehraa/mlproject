383b7d3aff6a4361d14d7707abdf657122062bc2
Nikhil Mehra <nikhilmehra845@gmail.com>
2025-11-09 20:48:01 +0100

    Deployment configuration

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -62,9 +62,9 @@ if __name__=="__main__":
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
-if __name__=="__main__":
-       obj = DataIngestion()
-       train_data,test_data = obj.initiate_data_ingestion()
+# if __name__=="__main__":
+#        obj = DataIngestion()
+#        train_data,test_data = obj.initiate_data_ingestion()
 
-       data_transformation = DataTransformation()
-       data_transformation.initiate_data_transformation(train_data,test_data)
+#        data_transformation = DataTransformation()
+#        data_transformation.initiate_data_transformation(train_data,test_data)
